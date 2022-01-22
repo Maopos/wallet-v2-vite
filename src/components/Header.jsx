3cdee8d7378c93be9ffab0fa98cda7dcdff595a7
maopos <mauricioposadamurillo@gmail.com>
2022-01-22 18:35:51 -0500

    mywallet-v2-vite

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="bg-green-700 text-center text-white text-2xl font-bold py-4">
+        My Wallet App
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
